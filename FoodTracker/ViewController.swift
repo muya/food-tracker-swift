0938d68a9526f7831e17e5352d4459f774227cd5
Muya <kingkonig@gmail.com>
2015-07-18 17:27:43 +0300

    choose image to accompany meal details
    - learnt how to choose image from camera roll

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,
+    UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let DEFAULT_MEAL_LABEL_TEXT = "Your Meal"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +38,60 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        mealNameLabel.text = textField.text
+        let trimmedText = textField.text?.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        if trimmedText != "" {
+            mealNameLabel.text = trimmedText
+            textField.text = trimmedText
+        }
+        else {
+            mealNameLabel.text = DEFAULT_MEAL_LABEL_TEXT
+            textField.text = ""
+        }
+    }
+    
+    // MARK UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // just dismiss the image picker
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: AnyObject]) {
+        // info dictionary contains multiple representations of the image, and
+        // this uses the original
+        print(info)
+        
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // display selected image in the ui
+        // set aspect ratio while at the same time respecting the image's ratio
+        photoImageView.contentMode = .ScaleAspectFit
+        photoImageView.image = selectedImage
+        
+        // dismiss the image picker
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
     // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        // allow user to pick media from photo library
+        let imagePickerController = UIImagePickerController()
+        
+        // make sure ViewController is notified when the user picks an image
+        imagePickerController.delegate = self
+        
+        // only allow photos to be picked, not taken
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // present the image picker
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
     @IBAction func setDefaultLabelText(sender: UIButton) {
-        mealNameLabel.text = "Default Text"
+        mealNameLabel.text = DEFAULT_MEAL_LABEL_TEXT
     }
     
 }
